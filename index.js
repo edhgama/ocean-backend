314fc8f73db298d37abb41bb07c4d18478e29b4e
Edheyberg Gama <62406729+edhgama@users.noreply.github.com>
2024-07-03 12:33:25 +0000

    get read all

--- index.js
+++ index.js
@@ -9,4 +9,13 @@ app.get('/oi', function (req, res) {
   res.send('Hello Worldddd 2')
 })
 
+//cria lista personagens
+const lista = ['Rick Sanches','Morty Smith', "Summer Smith"]
+
+// endpoint read all
+// GET /item
+app.get("/item", function(req,res){
+  res.send(lista)
+} )
+
 app.listen(3000)
